078d23aceb5e6d47d16a6eaeee673b952238f73b
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2016-06-12 18:25:13 +0200

    Added Dist::Zilla profile

---
+++ Dist-Zilla/Module.pm
@@ -0,0 +1,38 @@
+package {{$name}};
+
+use 5.022001;
+
+use strict;
+use warnings;
+use utf8;
+
+use English qw(-no_match_vars);
+use Const::Fast;
+use namespace::autoclean;
+
+our $VERSION = 0.001;
+
+=encoding utf8
+
+=head1 NAME
+
+{{$name}} - New module
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INSTANCE METHODS
+
+=cut
+
+
+
+=head1 CLASS METHODS
+
+=cut
+
+1; 
+
